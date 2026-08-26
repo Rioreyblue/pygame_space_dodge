21617cf6b709a213b774f6290bb584458f3029e2
Rioreyblue <franciscorey702@gmail.com>
2026-08-26 22:02:11 +0800

    added source of bg image

--- src/game.py
+++ src/game.py
@@ -6,6 +6,8 @@ WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Space Dodge")
 
+BG = pygame.image.load("images/bg.jpeg")
+
 def main():
     run = True
 
